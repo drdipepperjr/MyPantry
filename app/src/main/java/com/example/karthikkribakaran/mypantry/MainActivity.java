7070af0828d5e5ddbed43310bc6c1b1cdc64f275
Karthik Kribakaran <kkribakaran@gmail.com>
2017-12-05 08:26:37 -0800

    initialize project

---
+++ app/src/main/java/com/example/karthikkribakaran/mypantry/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.karthikkribakaran.mypantry;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends AppCompatActivity
+        implements MainMenu.OnFragmentInteractionListener,
+                    MetricsMainMenu.OnFragmentInteractionListener,
+                    MyGroceries.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment menu = new MainMenu();
+        getSupportFragmentManager().beginTransaction().add(android.R.id.content, menu).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
+    }
+}
